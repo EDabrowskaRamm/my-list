af6439013fa7deeaa21b777102385ffeb09ad6c2
Edyta Dabrowska-Ramm <editt.dabrowska@gmail.com>
2017-10-16 16:21:20 +0200

    add checkbox strikethrough

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, Input, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -7,52 +7,29 @@ import { Component, OnInit, ViewChild, Input, ElementRef, Renderer2 } from '@ang
 })
 export class ListComponent implements OnInit {
 
-  constructor(private _el: ElementRef, private _renderer: Renderer2) {  }
+  constructor() {  }
 
   @Input() input;
   title = 'Shopping list';
   items: any[] = [];
-  strike: false;
   selectedItem;
-  checked: any[] = [];
-  @ViewChild('itemText') itemText;
 
   ngOnInit() {   }
 
-// adding list items
+// add list items
   addItem(input: string) {
     if (input) {
       this.items.push(input);
+      // localStorage.setItem(item.value, input);
     }
-    console.log('added');
-  }
-// style done item
-  // getStyle() {
-  //   return this.strike ? 'line-through' : '';
-  // }
-  // onSelect($e, item): void {
-  //   this.selectedItem = item;
-  // }
-  updateChecked(item, event) {
-    const index = this.checked.indexOf(item);
-    if (event.target.checked) {
-      if (index === -1) {
-        this._renderer.addClass(this.itemText.nativeElement, 'selected');
-        // this.style.textDecoration = 'line-through';
-        this.checked.push(item);
-      }
-    } else {
-      this._renderer.removeClass(this.itemText.nativeElement, 'selected');
-      if (index !== -1) {
-        this.checked.splice(index, 1);
-      }
-    }
+    // console.log(localStorage);
   }
 // remove list item
   removeItem(item) {
     this.items = this.items.filter(j => j !== item);
     if (this.selectedItem === item) {
       this.selectedItem = null;
+      // localStorage.removeItem("lastname");
     }
   }
 
